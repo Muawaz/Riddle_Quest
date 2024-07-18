ffcd48589a82303e5dbfb68d7868e8ae88b893fe
Muawaz Saleem <muawazsaleem@gmail.com>
2024-07-18 12:37:33 +0500

    Add ContactUs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import NavBarComponent from './components/navbar';
 import Quiz from './components/Quiz';
 
 import Footer from './components/Footer'
 import About from './components/About'
+
 import './Scrollbar.css';
+
 import HeroSection from './components/HeroSection';
+import ConactUs from './components/ContactUs';
 
 
 function App() {
@@ -15,8 +19,10 @@ function App() {
       <NavBarComponent />
       <HeroSection />
       <div className="content">
+        
         <About />
         <Quiz/> 
+        <ConactUs/>
       </div>
         
       <Footer />
